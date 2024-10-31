c8973d4c6dd00979ed1a0d2e40bd67400aa0cd11
JoseRiosp <elberjose25@hotmail.com>
2024-10-31 16:00:52 -0500

    Update child.jsx

--- React-task/src/components/pure/child.jsx
+++ React-task/src/components/pure/child.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
-const Child = () => {
+const Child = ({name}) => {
   return (
     <div>
       <p>Child component</p>
+
     </div>
   )
 }
+
+// El Use Ref se usa para tomar un valor de un input y pasarlo a un nivel superior ejemplo: 
 
 export default Child
